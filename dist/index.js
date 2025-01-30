bab6dc26a10ac79bb604856c9a2185c023ab91e0
Dusan Plavak <dusan.plavak@gmail.com>
2025-01-30 17:29:18 +0800

    Update parameter nameing and add fail on missing PRs option, add examples

--- dist/index.js
+++ dist/index.js
@@ -31713,15 +31713,19 @@ const filterPullRequests = (data, options) => {
 
 
 const run = async () => {
-    const token = core.getInput("github-token", { required: true });
-    const onlyMerged = core.getInput("onlyMerged").toLowerCase() === 'true';
-    const targetBranch = core.getInput("targetBranch");
+    const token = core.getInput("github_token", { required: true });
+    const onlyMerged = core.getInput("only_merged").toLowerCase() === 'true';
+    const targetBranch = core.getInput("target_branch");
+    const failOnMissingPr = core.getInput("fail_on_missing_pr").toLowerCase() === 'true';
     const octokit = github.getOctokit(token);
     const repoName = github.context.repo.repo;
     const repoOwner = github.context.repo.owner;
     const commit_sha = github.context.sha;
     const pullRequests = await getPullRequests({ octokit, repoOwner, repoName, commit_sha });
     const filteredPullRequests = filterPullRequests(pullRequests, { onlyMerged, targetBranch });
+    if (failOnMissingPr && filteredPullRequests.length === 0) {
+        throw new Error("No PRs matching criteria were found!");
+    }
     core.setOutput("pullRequests", filteredPullRequests);
 };
 run().catch((err) => {
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import { getPullRequests } from './getPullRequests.js'
 import { filterPullRequests } from './filterPullRequests.js';
 
 const run = async () => {
-  const token = core.getInput("github-token", { required: true });
-  const onlyMerged = core.getInput("onlyMerged").toLowerCase() === 'true';
-  const targetBranch = core.getInput("targetBranch");
+  const token = core.getInput("github_token", { required: true })
+  const onlyMerged = core.getInput("only_merged").toLowerCase() === 'true'
+  const targetBranch = core.getInput("target_branch")
+  const failOnMissingPr = core.getInput("fail_on_missing_pr").toLowerCase() === 'true'
 
   const octokit = github.getOctokit(token)
 
@@ -16,6 +17,10 @@ const run = async () => {
 
   const pullRequests = await getPullRequests({octokit, repoOwner, repoName, commit_sha})
   const filteredPullRequests = filterPullRequests(pullRequests, { onlyMerged, targetBranch })
+
+  if (failOnMissingPr && filteredPullRequests.length === 0) {
+    throw new Error("No PRs matching criteria were found!")
+  }
 
   core.setOutput("pullRequests", filteredPullRequests)
 }
